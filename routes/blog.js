35184ae126ded93feaa4d9ad3fcbc5da3bbe8f45
jopoll87 <poll.jonathan87@gmail.com>
2023-07-03 13:42:43 -0600

    set up server connection. create and see all posts

---
+++ routes/blog.js
@@ -0,0 +1,38 @@
+const express = require("express");
+
+const db = require('../data/database');
+
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  res.redirect("/posts");
+});
+
+router.get("/posts", async (req, res) => {
+  const query = `SELECT posts.*, authors.name AS author_name FROM posts
+  INNER JOIN authors on posts.author_id = authors.id`;
+
+  const [posts] = await db.query(query)
+  res.render("posts-list", { posts: posts });
+});
+
+router.get("/new-post", async (req, res) => {
+    const [authors] = await db.query('SELECT * FROM authors');
+    res.render('create-post', { authors: authors});
+});
+
+router.post('/posts', async (req, res) => {
+    const data = [
+        req.body.title,
+        req.body.summary,
+        req.body.content,
+        req.body.author,
+    ];
+    await db.query('INSERT INTO posts (title, summary, body, author_id) VALUES (?)', [
+        data,
+    ]);
+
+    res.redirect('/posts');
+});
+
+module.exports = router;
